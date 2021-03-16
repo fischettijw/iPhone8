c9725c4ef2ec59fb3720afe9b20f19358ad3b6f5
Joseph Fischetti <fischettijw@gmail.com>
2021-03-16 08:43:58 -0400

    first

--- sketch.js
+++ sketch.js
@@ -1,16 +1,26 @@
-let x = 50;
-let y = 50;
+let circleDiam = 50;
+let xOrig = circleDiam / 2;
+let yOrig = circleDiam / 2;
+let xStep = 2;
+let yStep = 1;
+let x = xOrig;
+let y = yOrig;
+let iPhoneWidth = 375;
+let iPhonwHeight = 667;
+
 
 function setup() {
-    createCanvas(375, 667);
+    createCanvas(iPhoneWidth, iPhonwHeight);
     background(220);
+    noStroke();
 }
 
 function draw() {
     background(220);
-    circle(x, y, 50);
-    x++;
-    y++;
-    if (x > 325) { x = 50 };
-    if (y > 617) { y = 50 };
+    rect(0, 600, 375, 67);
+    circle(x, y, circleDiam);
+    x += xStep;
+    y += yStep;;
+    if (x > 350) { x = 50 };
+    if (y > 642) { y = 50 };
 }
